1e4ac01deb41c74385e945d619168bf302f7a2d2
luana <luanaesteve73@gmail.com>
2022-04-25 14:21:18 -0300

    codigooo

---
+++ js/script.js
@@ -0,0 +1,6 @@
+function mostrarEjercicios(){
+    document.getElementById('ejercicios').style.display = 'block';
+}
+function ocultarEjercicios(){
+    document.getElementById('ejercicios').style.display = 'none';
+}
